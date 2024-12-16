ffb0f2bb6c8a1b886dd72ac2caebd36570209e8e
kerichemman <jeromejkle@gmail.com>
2024-12-16 19:31:59 +0800

    New updates to build

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,14 @@ import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/triche-nest/' : '/',
-  build: {
-    outDir: 'dist',
-  },
+  base: '/triche-nest/',
   plugins: [
     react(),
     viteStaticCopy({
       targets: [
         {
-          src: '404.html',
-          dest: './',
+          src: 'public/404.html',
+          dest: '.',
         },
       ],
     }),
